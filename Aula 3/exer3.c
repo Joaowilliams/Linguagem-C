5cf649674fc9ca655d2002eb813b25f0b17dbf49
Joaowilliams <114708934+Joaowilliams@users.noreply.github.com>
2025-05-14 10:35:04 -0300

    Update exer3.c

--- Aula 3/exer3.c
+++ Aula 3/exer3.c
@@ -12,11 +12,21 @@ int main() {
     printf("Qual a sua altura?  ");
     scanf("%f", &altura);
 
-    imc = peso / (altura * altura);
+    printf("Seu IMC e: %2.f  ", imc);
 
-    switch (imc) {
-        case (Abaixo de 18.5: Abaixo do peso)
+    if (imc < 18.5) {
+        printf("Abaixo do peso\n");
+    } else if (imc < 25.0) {
+        printf("Peso normal\n");
+    } else if (imc < 30.0) {
+        printf("Sobrepeso");
+    } else if (imc < 35.0) {
+        printf("Obesidade grau 1\n");
+    } else if (imc < 40.0) {
+        printf("Obesidade grau 2\n");
+    } else {
+        printf("Obesidade grau 3\n");
     }
-
+    
     return 0;
 }
