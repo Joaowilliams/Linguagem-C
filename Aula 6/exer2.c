d439b0bdeefd67ee4cf648dd8873bea375cff85d
Joaowilliams <114708934+Joaowilliams@users.noreply.github.com>
2025-05-23 12:10:23 -0300

    Add files via upload

---
+++ Aula 6/exer2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+void Decrescente (int n) {
+    if ( n == 0) {
+        return;
+    }
+
+    printf("%d", n);
+    Decrescente ( n - 1);
+    
+}
+
+int main() {
+
+    int N;
+    printf("Informe o valor de N:  ");
+    scanf("%d", &N);
+
+    printf("Numero de 1 ate %d  ", N);
+    Decrescente(N);
+
+    return 0;
+}
