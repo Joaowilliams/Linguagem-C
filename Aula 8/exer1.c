719b0e724008321d41175516e18bd86770f3e6c7
Joaowilliams <114708934+Joaowilliams@users.noreply.github.com>
2025-06-02 11:42:40 -0300

    Update exer1.c

--- Aula 8/exer1.c
+++ Aula 8/exer1.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 
 int main() {
-
     int numeros[3][3];
     int soma = 0;
-    for (int i = 0; i < 3; i++) {
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("Digite um numero  ");
-            scanf("%d", &numeros[i][j]);
-        }
-    }
-    
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            soma = soma + numeros[i][j];
-            
-        } 
 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Digite o valor da posicao [%d][%d]: ", i, j);
+            scanf("%d", &numeros[i][j]);
+            soma = soma + numeros[i][j];
+        } 
     }
-        printf("A soma de todos os valores da matriz e: %d\n", soma);
+
+
+    printf("A soma de todos os valores da matriz e: %d\n", soma);
     
-    return 0;
 
     return 0;
 }
